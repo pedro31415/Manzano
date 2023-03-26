8399f85eb407f33f04851c0d0c586ce449972194
Pedro <pedrogoku80@gmail.com>
2023-03-26 10:52:51 -0300

    Resolvendo problema

--- algoritmo_d.c
+++ algoritmo_d.c
@@ -5,11 +5,10 @@ int main(){
   setlocale(LC_ALL, "Portuguese");
   float v,t,d,c;
   printf("informe a velocidade do automóvel e o tempo gasto percorrido: ");
-  scanf("%f %f, &v, &t);      
+  scanf("%f %f", &v, &t);   
   d = v*t;
   c = d/12;
-  
-  printf("A quantidade de litros gatos de combustível a cada 12km percorridos é %.2f", c);
+  printf("o combústivel gasto em 12km percorrido é %.2f", c);   
   
  return 0; 
 }
