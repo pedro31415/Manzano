3c367131a7fa46182ad40229b769da776449b3c8
Pedro <pedrogoku80@gmail.com>
2023-03-26 21:13:18 -0300

    Correção de valor

--- algoritmos_a.c
+++ algoritmos_a.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <locale.h>
 
 int main(){
+    setlocale(LC_ALL,"Portuguese");
     float c,f;
-    printf("Informe o valor de celsius para convers√£o em  Fahrenheit ");
+    printf("Informe o valor de celsius para convers„o em  Fahrenheit: ");
     scanf("%f", &c);
-    f = (9*c + 160)/2;
+    f = (9*c + 160)/5;
 
-    printf("o valor convertido %.2f, √© igual: %.2f", c,f);
+    printf("o valor convertido %.2f, È a igual: %.2f", c,f);
 
     return 0;
 }
