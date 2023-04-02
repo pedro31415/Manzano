3ee914f10bcc34d1f0a6523505482441923be9fc
Pedro <pedrogoku80@gmail.com>
2023-04-01 21:39:51 -0300

    Prestação atrasada

---
+++ algoritmo_e.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    double v,t,tf,p;
+    printf("O valor da prestação, a taxa do valor e o tempo de atraso: ");
+    scanf("%lf%lf%lf", &v,&t,&tf);
+    p = v +(v*(t/100)*tf);
+    printf("A prestação de atraso: %.2lf", p);
+
+    return 0;
+}
