cb63c1c8e092d5fb017f6d4be53392e052e1768e
Pedro <pedrogoku80@gmail.com>
2023-04-01 22:00:11 -0300

    Troca de valores

---
+++ algoritmo_f.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+     int a,b,aux;
+     printf("informe um valor(a): ");
+     scanf("%d", &a);
+     printf("informe um valor(b): ");
+     scanf("%d", &b);
+     aux = a;
+      a = b;
+      b = aux;
+
+     printf("Os valores alterados a = %d\t b=%d\n", a,b);
+
+    return 0;
+}
