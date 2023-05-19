884b2268fefd8160aab671522b5667c8b7a8d064
Pedro <pedrogoku80@gmail.com>
2023-05-19 18:56:09 -0300

    Capitulo 5

---
+++ manzano_cap5/algoritmo_i.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    int x = 1;
+    int y = 0;
+    int proxtermo;
+    for(int i=0; i<15; i++){
+       printf("%d ", y);
+       proxtermo  = x+y;
+       y = x;
+       x = proxtermo;
+    }
+    
+
+    return 0;
+}
